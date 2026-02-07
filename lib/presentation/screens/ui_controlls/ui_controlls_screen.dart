97fde7e2d14a3bed04d8302238c0f5db57a048d7
ObiTobi64 <obitotcp@gmail.com>
2026-02-07 02:09:13 -0400

    screens creadas y barril actualizado

---
+++ lib/presentation/screens/ui_controlls/ui_controlls_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UIControllsScreen extends StatelessWidget {
+
+  static const String name = 'ui_controlls_screen';
+  const UIControllsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
